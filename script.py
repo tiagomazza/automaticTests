135408bfd494fd0094ccc231953ee150aad4e13e
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2025-01-20 23:48:57 +0000

    Atualizar o script.py

--- script.py
+++ script.py
@@ -12,10 +12,3 @@ def check_phrase_in_site(url, phrase):
         print(f"Erro ao acessar o site: {e}")
         return False
 
-if __name__ == "__main__":
-    url = "https://aborgesdoamaral.pt"
-    phrase = "tudo o que precisa para"
-    if check_phrase_in_site(url, phrase):
-        print("Frase encontrada!")
-    else:
-        print("Frase não encontrada.")
