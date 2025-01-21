619d8515732fd4d14ca3bb831eafaf95047e098c
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2025-01-21 00:28:46 +0000

    Atualizar o test_script.py

--- test_script.py
+++ test_script.py
@@ -1,14 +1,7 @@
 import pytest
 import requests
 import datetime
-from script import check_phrase_in_site  # Certifique-se de que essa função existe no arquivo script.py
 
-# Fixture para capturar o tempo de execução
-@pytest.fixture(scope="session")
-def execution_time():
-    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-# Função para verificar o status da página
 def check_page_status(url):
     """Função que verifica se a página foi carregada corretamente, retornando True se a resposta for 200 (OK)."""
     try:
@@ -24,22 +17,15 @@ def test_page_status(url):
     result = check_page_status(url)
     assert result is True, f"Página não foi aberta corretamente. URL: {url}"
 
-# Teste para verificar se a frase está presente na página
-@pytest.mark.parametrize("url, phrase", [("https://aborgesdoamaral.streamlit.app", "Ponto")])
-def test_phrase_found(url, phrase):
-    """Teste para verificar se a frase está presente no site."""
-    result = check_phrase_in_site(url, phrase)
-    assert result is True, f"Frase '{phrase}' não encontrada na página. URL: {url}"
-
 # Fixture para gerar o relatório de teste
 @pytest.fixture(scope="session", autouse=True)
 def generate_report(request):
     yield
     report_content = f"""
     <html>
-    <head><title>Relatório de Teste - Verificação de Status da Página e Frase</title></head>
+    <head><title>Relatório de Teste - Verificação de Status da Página</title></head>
     <body>
-    <h1>Relatório de Teste - Verificação de Status da Página e Frase</h1>
+    <h1>Relatório de Teste - Verificação de Status da Página</h1>
     <p>Data e hora da execução: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</p>
     <h2>Resultados:</h2>
     <ul>
